e273bca21da6c82da90cfe116d9da70d2864d696
Tanel <tanel.prikk@hotmail.com>
2017-04-26 18:39:36 +0300

    fixes

--- src/main/java/ee/ttu/idk0071/sentiment/Application.java
+++ src/main/java/ee/ttu/idk0071/sentiment/Application.java
@@ -3,6 +3,9 @@ package ee.ttu.idk0071.sentiment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
+
+@EnableEmailTools
 @SpringBootApplication
 public class Application {
 
